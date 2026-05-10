82b81767a71ca939f72e538f5bb2ce9943676183
Raul <rs6626808@gmail.com>
2026-05-09 22:19:22 -0500

    Ejercicio03 Factory: Mejora en la validacion de Monto

--- 01-Creacionales/Factory/Ejercicio03-SistemaDePagos/src/Creatores/FormasPagoCreator.java
+++ 01-Creacionales/Factory/Ejercicio03-SistemaDePagos/src/Creatores/FormasPagoCreator.java
@@ -5,15 +5,14 @@ import Productos.MetodoPago;
 public abstract class FormasPagoCreator {
     public abstract MetodoPago crearMetodoPago();
     public void emitirpago(double monto){
+        if(!verificarMonto(monto)){
+            System.out.println("No se puede procesar el monto");
+            return;
+        }
         MetodoPago metodoPago = crearMetodoPago();
         metodoPago.procesarPago(monto);
     }
-    public void verificarMonto(double monto){
-        if(0>= monto){
-            System.out.println("No se procesor el pago");
-        }
-        else {
-            emitirpago(monto);
-        }
+    public boolean verificarMonto(double monto){
+        return monto > 0;
     }
 }
--- 01-Creacionales/Factory/Ejercicio03-SistemaDePagos/src/Main.java
+++ 01-Creacionales/Factory/Ejercicio03-SistemaDePagos/src/Main.java
@@ -1,11 +1,14 @@
 import Creatores.FormasPagoCreator;
 import Creatores.PaypalCreator;
+import Creatores.PlinCreator;
 
 public class Main {
     public static void main(String[] args) {
         FormasPagoCreator factory;
         factory = new PaypalCreator();
-        factory.verificarMonto(-10);
+        factory.emitirpago(-10);
+        factory = new PlinCreator();
+        factory.emitirpago(200);
 
     }
 }
