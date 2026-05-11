2a308fe48a730a737d32d210a6a6af43863a8e62
Raul <rs6626808@gmail.com>
2026-05-11 17:47:03 -0500

    Ejercicio06 Factory: CreadorPrincipal completa

--- 01-Creacionales/Factory/Ejercicio06-SistemaTicketsSoporte/src/Creators/TicketsCreator.java
+++ 01-Creacionales/Factory/Ejercicio06-SistemaTicketsSoporte/src/Creators/TicketsCreator.java
@@ -6,9 +6,26 @@ import Productos.TicketSoporte;
 public abstract class TicketsCreator {
     public abstract TicketSoporte createTicketSoporte();
     public void procesarSolicitudTicket(SolicitudTicket solicitud) {
+        if (!validarSolicitudTicket(solicitud)) {
+            System.out.println("Error al procesar SolicitudTicket");
+            return;
+        }
+        TicketSoporte ticket = createTicketSoporte();
+
+        ticket.atenderTicket(solicitud);
+        ticket.calcularPrioridad(solicitud);
+        ticket.mostrarResumen();
+
+
 
     }
     public boolean validarSolicitudTicket(SolicitudTicket solicitud) {
+        if (solicitud == null) {
+            return false;
+        }
+        if (!solicitud.isValido()){
+            return false;
+        }
         return true;
     }
 }
