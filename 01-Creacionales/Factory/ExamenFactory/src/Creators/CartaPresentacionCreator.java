301cc64be43309ed252f3bfece3b1560121665e3
Raul <rs6626808@gmail.com>
2026-05-12 12:25:47 -0500

    avance en validaciones especiales

--- 01-Creacionales/Factory/ExamenFactory/src/Creators/CartaPresentacionCreator.java
+++ 01-Creacionales/Factory/ExamenFactory/src/Creators/CartaPresentacionCreator.java
@@ -1,4 +1,25 @@
 package Creators;
 
-public class CartaPresentacionCreator {
+import Modelos.SolicitudDocumento;
+import Productos.CartaPresentacion;
+import Productos.DocumentoAcademico;
+
+public class CartaPresentacionCreator extends DocumentoCreator{
+    @Override
+    public DocumentoAcademico crearDocumento() {
+        return new CartaPresentacion();
+    }
+
+    @Override
+    public boolean validarSolicitud(SolicitudDocumento solicitud) {
+        if(!super.validarSolicitud(solicitud)){
+            return false;
+        }
+        if(solicitud.getCiclo()<5){
+            System.out.println("El ciclo debe ser mayor a 5.");
+            return false;
+        }
+        if(solicitud.getMotivo().toLowerCase().contains("practicas")){}
+        return true;
+    }
 }
