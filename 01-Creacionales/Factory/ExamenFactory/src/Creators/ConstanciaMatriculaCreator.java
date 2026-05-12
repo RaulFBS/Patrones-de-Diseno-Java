f223dbd7cc63ecbb83abe72fd863c8a832d37a7f
Raul <rs6626808@gmail.com>
2026-05-12 12:17:01 -0500

    completado con validaciones especiales

--- 01-Creacionales/Factory/ExamenFactory/src/Creators/ConstanciaMatriculaCreator.java
+++ 01-Creacionales/Factory/ExamenFactory/src/Creators/ConstanciaMatriculaCreator.java
@@ -1,4 +1,28 @@
 package Creators;
 
-public class ConstanciaMatriculaCreator {
+import Modelos.SolicitudDocumento;
+import Productos.ConstanciaMatricula;
+import Productos.DocumentoAcademico;
+
+public class ConstanciaMatriculaCreator extends DocumentoCreator{
+    @Override
+    public DocumentoAcademico crearDocumento() {
+        return new ConstanciaMatricula();
+    }
+
+    @Override
+    public boolean validarSolicitud(SolicitudDocumento solicitud) {
+        if(!super.validarSolicitud(solicitud)){
+            return false;
+        }
+        if(!solicitud.isMatriculado()){
+            System.out.println("El estudiante no esta matriculado.");
+            return false;
+        }
+        if(solicitud.getCiclo()<1){
+            System.out.println("El estudiante no esta ciclo.");
+            return false;
+        }
+        return true;
+    }
 }
