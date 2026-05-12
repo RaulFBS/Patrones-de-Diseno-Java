be19c244d665a1ac64051cb6de8111ead226e473
Raul <rs6626808@gmail.com>
2026-05-11 22:24:21 -0500

    Ejercicio06 Factory: forma de calculo de prioridad actualizado

--- 01-Creacionales/Factory/Ejercicio06-SistemaTicketsSoporte/src/Productos/TicketCuenta.java
+++ 01-Creacionales/Factory/Ejercicio06-SistemaTicketsSoporte/src/Productos/TicketCuenta.java
@@ -7,12 +7,21 @@ public class TicketCuenta implements TicketSoporte{
     private SolicitudTicket solicitud;
     @Override
     public void atenderTicket(SolicitudTicket solicitud) {
+        this.solicitud = solicitud;
         System.out.println("ATENDIENDO TICKET DE SOPORTE DE CUENTA");
     }
 
     @Override
     public void calcularPrioridad(SolicitudTicket solicitud) {
-        
+        if(solicitud.getCategoria().equals("seguridad")){
+            prioridad="Alta";
+        }
+        else if(solicitud.getCategoria().equals("cuenta")){
+            prioridad="Media";
+        }
+        else if(solicitud.getCategoria().equals("login")){
+            prioridad="Baja";
+        }
     }
 
     @Override
