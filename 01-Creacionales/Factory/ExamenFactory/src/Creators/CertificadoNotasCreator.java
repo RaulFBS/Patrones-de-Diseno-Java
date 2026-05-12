fffdad5c31c6f6ec3ae0142db7e0a453c09b67dc
Raul <rs6626808@gmail.com>
2026-05-12 12:21:45 -0500

    completado con validaciones especiales

--- 01-Creacionales/Factory/ExamenFactory/src/Creators/CertificadoNotasCreator.java
+++ 01-Creacionales/Factory/ExamenFactory/src/Creators/CertificadoNotasCreator.java
@@ -1,4 +1,25 @@
 package Creators;
 
-public class CertificadoNotasCreator {
+import Modelos.SolicitudDocumento;
+import Productos.CertificadoNotas;
+import Productos.DocumentoAcademico;
+
+public class CertificadoNotasCreator extends DocumentoCreator{
+    @Override
+    public DocumentoAcademico crearDocumento() {
+        return new CertificadoNotas();
+    }
+
+    @Override
+    public boolean validarSolicitud(SolicitudDocumento solicitud) {
+        if(!super.validarSolicitud(solicitud)){
+            return false;
+        }
+        if (solicitud.getPromedio()<0 && solicitud.getPromedio()>20){
+            System.out.println("El promedio no debe ser mayor o igual a 0 y no puede ser mayor a 20");
+            return false;
+        }
+
+        return true;
+    }
 }
