2776d5879b538d9a4e018f5975e669b3106416c9
Raul <rs6626808@gmail.com>
2026-05-11 22:36:34 -0500

    Ejercicio06 Factory: Terminar metodo calcularPrioridad

--- 01-Creacionales/Factory/Ejercicio06-SistemaTicketsSoporte/src/Productos/TicketFacturacion.java
+++ 01-Creacionales/Factory/Ejercicio06-SistemaTicketsSoporte/src/Productos/TicketFacturacion.java
@@ -3,13 +3,17 @@ package Productos;
 import Modelos.SolicitudTicket;
 
 public class TicketFacturacion implements TicketSoporte{
+    private SolicitudTicket solicitud;
+    private String prioridad;
     @Override
     public void atenderTicket(SolicitudTicket solicitud) {
-
+        System.out.println("ATENDIENDO TICKET DE SOPORTE DE CUENTA");
     }
 
     @Override
     public void calcularPrioridad(SolicitudTicket solicitud) {
+        //Agregar el calculo dependiendo si es cliente premium o no
+        
 
     }
 
