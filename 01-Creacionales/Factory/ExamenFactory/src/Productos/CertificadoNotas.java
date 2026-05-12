c7a46089d772c05eb58b655bba1774562e8ac21b
Raul <rs6626808@gmail.com>
2026-05-12 11:14:34 -0500

    Examen Factory: clase certificado notas completo

--- 01-Creacionales/Factory/ExamenFactory/src/Productos/CertificadoNotas.java
+++ 01-Creacionales/Factory/ExamenFactory/src/Productos/CertificadoNotas.java
@@ -3,18 +3,24 @@ package Productos;
 import Modelos.SolicitudDocumento;
 
 public class CertificadoNotas implements DocumentoAcademico{
+    SolicitudDocumento solicitud;
+    private double precio;
     @Override
     public void generarDocumento(SolicitudDocumento solicitud) {
-
+        System.out.println("Generando  Certificado Notas");
     }
 
     @Override
     public void calcularCosto(SolicitudDocumento solicitud) {
-
+        System.out.println("Calculando  Costo");
+        precio = 150;
     }
 
     @Override
     public void mostrarResumen() {
+        System.out.println("Mostrando Resumen");
+        System.out.println(solicitud);
+        System.out.println("Precio: "+precio);
 
     }
 }
