a082192ea83131a7555c6ffb6d4ca2d69cfc7f9d
Raul <rs6626808@gmail.com>
2026-05-13 10:10:59 -0500

    completado validacion especial

--- 01-Creacionales/Factory/ExamenFactory/src/Creators/SolicitudBecaCreator.java
+++ 01-Creacionales/Factory/ExamenFactory/src/Creators/SolicitudBecaCreator.java
@@ -1,4 +1,27 @@
 package Creators;
 
-public class SolicitudBecaCreator {
+import Modelos.SolicitudDocumento;
+import Productos.DocumentoAcademico;
+import Productos.SolicitudBeca;
+
+public class SolicitudBecaCreator extends DocumentoCreator {
+    @Override
+    public DocumentoAcademico crearDocumento() {
+        return null;
+    }
+
+    @Override
+    public boolean validarSolicitud(SolicitudDocumento solicitud) {
+        if(!super.validarSolicitud(solicitud)){
+            return false;
+        }
+        if(solicitud.getPromedio()<15){
+            System.out.println("El promedio debe ser mayor a 15");
+            return false;
+        }
+        if(!solicitud.isMatriculado()){
+            return false;
+        }
+        return true;
+    }
 }
