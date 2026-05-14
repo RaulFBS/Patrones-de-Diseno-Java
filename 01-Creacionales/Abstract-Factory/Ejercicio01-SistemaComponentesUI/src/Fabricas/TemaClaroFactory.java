da9aa4ea169213b7941cbe45e0a16c7da1ab9138
Raul <rs6626808@gmail.com>
2026-05-14 07:57:46 -0500

    Ejercicio01 AbstracFactory: TemaClaroFactory implementando AbstractFactory

--- 01-Creacionales/Abstract-Factory/Ejercicio01-SistemaComponentesUI/src/Fabricas/TemaClaroFactory.java
+++ 01-Creacionales/Abstract-Factory/Ejercicio01-SistemaComponentesUI/src/Fabricas/TemaClaroFactory.java
@@ -1,4 +1,25 @@
 package Fabricas;
 
-public class TemaClaroFactory {
+import Productos.Boton.Boton;
+import Productos.Boton.BotonClaro;
+import Productos.CajaTexto.CajaTexto;
+import Productos.CajaTexto.CajaTextoClaro;
+import Productos.Checkbox.CheckBox;
+import Productos.Checkbox.CheckBoxClaro;
+
+public class TemaClaroFactory implements AbstractFactory{
+    @Override
+    public Boton createBoton() {
+        return new BotonClaro();
+    }
+
+    @Override
+    public CheckBox createCheckBox() {
+        return new CheckBoxClaro();
+    }
+
+    @Override
+    public CajaTexto createCajaTexto() {
+        return new CajaTextoClaro();
+    }
 }
