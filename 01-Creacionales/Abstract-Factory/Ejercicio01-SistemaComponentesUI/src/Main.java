55a14508f76c3b26a038c6a82e88c6cde6c0279f
Raul <rs6626808@gmail.com>
2026-05-14 08:10:51 -0500

    Ejercicio01 AbstracFactory: Completado

--- 01-Creacionales/Abstract-Factory/Ejercicio01-SistemaComponentesUI/src/Main.java
+++ 01-Creacionales/Abstract-Factory/Ejercicio01-SistemaComponentesUI/src/Main.java
@@ -1,5 +1,12 @@
+import Cliente.Cliente;
+import Fabricas.AbstractFactory;
+import Fabricas.TemaClaroFactory;
+
 public class Main {
     static void main() {
+        AbstractFactory factory = new TemaClaroFactory();
+        Cliente cliente = new Cliente(factory);
+        cliente.crearComponenteUI();
 
     }
 }
