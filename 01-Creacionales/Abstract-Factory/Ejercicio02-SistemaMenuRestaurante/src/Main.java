09d249917e38709047fb714e9a8f1c4280d66218
Raul <rs6626808@gmail.com>
2026-05-14 10:48:14 -0500

    Ejercicio02 AbstractFactory: Completado

--- 01-Creacionales/Abstract-Factory/Ejercicio02-SistemaMenuRestaurante/src/Main.java
+++ 01-Creacionales/Abstract-Factory/Ejercicio02-SistemaMenuRestaurante/src/Main.java
@@ -1,5 +1,13 @@
+import Cliente.Cliente;
+import Fabricas.RestauranteAF;
+import Fabricas.RestaurantePeruanoF;
+
 public class Main {
     static void main() {
+        RestauranteAF fabrica = new RestaurantePeruanoF();
+
+        Cliente cliente = new Cliente(fabrica);
+        cliente.crearRestaurante();
 
     }
 }
