c65918e9d40387a9ecaa39c21206c634ff8857ec
Raul <rs6626808@gmail.com>
2026-05-14 07:57:56 -0500

    Ejercicio01 AbstracFactory: TemaOscuroFactory implementando AbstractFactory

--- 01-Creacionales/Abstract-Factory/Ejercicio01-SistemaComponentesUI/src/Fabricas/TemaOscuroFactory.java
+++ 01-Creacionales/Abstract-Factory/Ejercicio01-SistemaComponentesUI/src/Fabricas/TemaOscuroFactory.java
@@ -1,4 +1,25 @@
 package Fabricas;
 
-public class TemaOscuroFactory {
+import Productos.Boton.Boton;
+import Productos.Boton.BotonOscuro;
+import Productos.CajaTexto.CajaTexto;
+import Productos.CajaTexto.CajaTextoOscuro;
+import Productos.Checkbox.CheckBox;
+import Productos.Checkbox.CheckBoxOscuro;
+
+public class TemaOscuroFactory implements AbstractFactory{
+    @Override
+    public Boton createBoton() {
+        return new BotonOscuro();
+    }
+
+    @Override
+    public CheckBox createCheckBox() {
+        return new CheckBoxOscuro();
+    }
+
+    @Override
+    public CajaTexto createCajaTexto() {
+        return new CajaTextoOscuro();
+    }
 }
