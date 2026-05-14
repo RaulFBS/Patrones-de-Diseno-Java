32f2597e21b3c937169b5b2b13d28180dcbce12a
Raul <rs6626808@gmail.com>
2026-05-14 08:10:21 -0500

    Ejercicio01 AbstracFactory: creando y completando logica de cliente

--- 01-Creacionales/Abstract-Factory/Ejercicio01-SistemaComponentesUI/src/Cliente/Cliente.java
+++ 01-Creacionales/Abstract-Factory/Ejercicio01-SistemaComponentesUI/src/Cliente/Cliente.java
@@ -1,4 +1,27 @@
 package Cliente;
 
+import Fabricas.AbstractFactory;
+import Productos.Boton.Boton;
+import Productos.CajaTexto.CajaTexto;
+import Productos.Checkbox.CheckBox;
+
 public class Cliente {
+    private AbstractFactory factory;
+    private Boton boton;
+    private CajaTexto cajaTexto;
+    private CheckBox checkBox;
+
+    public Cliente(AbstractFactory factory) {
+        this.factory = factory;
+    }
+    public void crearComponenteUI(){
+        boton = factory.createBoton();
+        checkBox = factory.createCheckBox();
+        cajaTexto = factory.createCajaTexto();
+
+        boton.presionar();
+        checkBox.marcar();
+        cajaTexto.escribir();
+    }
+
 }
