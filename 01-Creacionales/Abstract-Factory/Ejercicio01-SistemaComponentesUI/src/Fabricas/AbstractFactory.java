ce1f394c77fd6cceb7983a6f57d211e76971e03c
Raul <rs6626808@gmail.com>
2026-05-14 07:55:25 -0500

    Ejercicio01 AbstracFactory: AbstractFactory completa

--- 01-Creacionales/Abstract-Factory/Ejercicio01-SistemaComponentesUI/src/Fabricas/AbstractFactory.java
+++ 01-Creacionales/Abstract-Factory/Ejercicio01-SistemaComponentesUI/src/Fabricas/AbstractFactory.java
@@ -1,4 +1,12 @@
 package Fabricas;
 
+import Productos.Boton.Boton;
+import Productos.CajaTexto.CajaTexto;
+import Productos.Checkbox.CheckBox;
+
 public interface AbstractFactory {
+    Boton createBoton();
+    CheckBox createCheckBox();
+    CajaTexto createCajaTexto();
+
 }
