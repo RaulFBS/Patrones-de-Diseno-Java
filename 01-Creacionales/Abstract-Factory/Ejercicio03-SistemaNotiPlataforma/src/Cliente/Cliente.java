72dc07dc0cd7a26d0589338904b3ebc55b88c364
Raul <rs6626808@gmail.com>
2026-05-14 12:07:40 -0500

    Ejercicio03 AbstracFactory: Clinete y main completados

---
+++ 01-Creacionales/Abstract-Factory/Ejercicio03-SistemaNotiPlataforma/src/Cliente/Cliente.java
@@ -0,0 +1,26 @@
+package Cliente;
+
+import Fabricas.NotificacionFactory;
+import Productos.Icono.Icono;
+import Productos.Notificacion.Notificacion;
+import Productos.Sonido.Sonido;
+
+public class Cliente {
+    private NotificacionFactory factory;
+    private Icono icono;
+    private Notificacion notificacion;
+    private Sonido sonido;
+    public Cliente(NotificacionFactory factory) {
+        this.factory = factory;
+    }
+
+    public void crearNotificacion() {
+        icono = factory.createIcono();
+        sonido = factory.createSonido();
+        notificacion = factory.createNotificacion();
+
+        icono.mostrar();
+        sonido.reproducir();
+        notificacion.enviar();
+    }
+}
--- 01-Creacionales/Abstract-Factory/Ejercicio03-SistemaNotiPlataforma/src/Main.java
+++ 01-Creacionales/Abstract-Factory/Ejercicio03-SistemaNotiPlataforma/src/Main.java
@@ -1,2 +1,11 @@
+import Cliente.Cliente;
+import Fabricas.AndroidFactory;
+import Fabricas.NotificacionFactory;
+
 public class Main {
+    public static void main(String[] args) {
+        NotificacionFactory factory = new AndroidFactory() ;
+        Cliente cliente = new Cliente(factory);
+        cliente.crearNotificacion();
+    }
 }
