1d9fd2fc6630a0bbd0b18342f94953448809cb55
Raul <rs6626808@gmail.com>
2026-05-15 17:26:55 -0500

    Ejercicio04 AbstractFactory: Ejericicio Completado

--- 01-Creacionales/Abstract-Factory/Ejercicio04-SistemaVehiculos/src/Main.java
+++ 01-Creacionales/Abstract-Factory/Ejercicio04-SistemaVehiculos/src/Main.java
@@ -1,2 +1,13 @@
+import Fabricas.VehiculosElectricosFactory;
+import Fabricas.VehiculosFactory;
+
+import Sistema.SistemaVehiculos;
+
 public class Main {
+    static void main() {
+        VehiculosFactory factory = new VehiculosElectricosFactory();
+        SistemaVehiculos cliente = new   SistemaVehiculos(factory);
+        cliente.probarVehiculo();
+    }
+
 }
