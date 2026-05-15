3d749d8f5e9174b28ddab7bdf09fbc262773f7e5
Raul <rs6626808@gmail.com>
2026-05-15 16:37:08 -0500

    Ejercicio04 AbstractFactory: clase padre vehiculo completa

--- 01-Creacionales/Abstract-Factory/Ejercicio04-SistemaVehiculos/src/Productos/Vehiculo.java
+++ 01-Creacionales/Abstract-Factory/Ejercicio04-SistemaVehiculos/src/Productos/Vehiculo.java
@@ -1,4 +1,19 @@
 package Productos;
 
-public class Vehiculo {
+public abstract class Vehiculo {
+    protected String modelo;
+    protected String tipoEnergia;
+    protected int velocidadMaxima;
+
+    public Vehiculo(String modelo, String tipoEnergia, int velocidadMaxima) {
+        this.modelo = modelo;
+        this.tipoEnergia = tipoEnergia;
+        this.velocidadMaxima = velocidadMaxima;
+    }
+
+    public void mostrarDatos(){
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Tipo Energia: " + tipoEnergia);
+        System.out.println("Velocidad Maxima: " + velocidadMaxima + "Km/h");
+    }
 }
